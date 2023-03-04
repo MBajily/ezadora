97d4e500d1d35359acc80915a1a5e5f228ec1dc4
Mohammed Elgaily <mbajily@gmail.com>
2023-03-04 07:06:04 +0200

    Work on Cart page and its views.

--- store/views.py
+++ store/views.py
@@ -12,8 +12,15 @@ def collection(request):
 
 
 def cart(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items = []
 	
-	context = {}
+	context = {'items':items}
 
 	return render(request, 'store/cart.html', context)
 
