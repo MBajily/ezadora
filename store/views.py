a87396a563eaf4d47606034ec4ffc9f7977f5f15
Mohammed Elgaily <mbajily@gmail.com>
2023-03-07 13:09:17 +0200

    Create Checkout page and views.

--- store/views.py
+++ store/views.py
@@ -27,7 +27,15 @@ def cart(request):
 
 
 def checkout(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		order = {'get_cart_total': 0, 'get_cart_items': 0}
+		items = []
 	
-	context = {}
+	context = {'items': items, 'order': order}
 
 	return render(request, 'store/checkout.html', context)
